b00ce1d2a0dcc351d209a27847a4aa46363212a7
shivanit16 <shivaniebzar@gmail.com>
2021-04-19 23:58:50 +0530

    completed half will do the rest tomo mrng

--- appp1/views.py
+++ appp1/views.py
@@ -3,13 +3,7 @@ from .models import appp1
 # Create your views here.
 
 def funs1(request):
-    results =list()
     allobj = appp1.objects.all()
-    
-    for i in allobj:
-        each = dict(Name=i.Name, StageName=i.StageName, Songs=i.Songs)
-        results.append(each)
-    context = {'appp1' : results}
-    return render(request,'first.html', context = context)
+    return render(request,'first.html', {'results' : allobj})
 
 
