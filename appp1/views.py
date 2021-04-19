5f7d7b98388854c2aec695d6904b04bca5f42ae3
shivanit16 <shivaniebzar@gmail.com>
2021-04-19 12:28:38 +0530

    changed some mistakes from yesterdat

--- appp1/views.py
+++ appp1/views.py
@@ -5,11 +5,10 @@ from .models import appp1
 def funs1(request):
     results =list()
     allobj = appp1.objects.all()
-    a = appp1(Name="Kim Taehyung")
+    
     for i in allobj:
         each = dict(Name=i.Name, StageName=i.StageName, Songs=i.Songs)
         results.append(each)
-    print (results)
     context = {'appp1' : results}
     return render(request,'first.html', context = context)
 
