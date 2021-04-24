56f17066459d851106fb0f0d7feada54733a8f30
shivanit16 <shivaniebzar@gmail.com>
2021-04-24 16:45:35 +0530

    done

--- appp2/models.py
+++ appp2/models.py
@@ -5,7 +5,9 @@ from django.db import models
 class appp2(models.Model):
     Name = models.CharField(max_length=100)
     Bias = models.CharField(max_length=50)
+    Age  = models.IntegerField(max_length = 200)
+    Country = models.TextField(max_length=50)
 
 
     def __str__(self):
-        return self.Name + "-----"  + self.Bias + "-----" 
+        return self.Name + "-----"  + self.Bias + "-----"  + self.Age + self.Country
--- appp2/views.py
+++ appp2/views.py
@@ -7,11 +7,10 @@ def cans1(request):
     if request.method == 'POST':
         Name = request.POST['name']
         Bias = request.POST['bias']
+        Age = request.POST['age']
         Country = request.POST['country']
-        print(Name)
-        print(Bias)
 
-        allobj2 = appp2(Name= Name,Bias= Bias)
+        allobj2 = appp2(Name= Name,Bias= Bias,Age=Age, Country= Country)
         allobj2.save()
 
     allobj2 = appp2.objects.all()
